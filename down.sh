ac6e0ac0a7989ca2659406f8f4031c5280fd493a
Peter-Frank Spierenburg <spierepf@hotmail.com>
2017-01-24 11:50:46 -0400

    Renamed client to transmitter

--- down.sh
+++ down.sh
@@ -2,6 +2,6 @@
 
 docker-compose down
 docker rm securesnmp_server_1
-docker rm securesnmp_client_1
+docker rm securesnmp_transmitter_1
 docker rmi securesnmp_server
-docker rmi securesnmp_client
+docker rmi securesnmp_transmitter
